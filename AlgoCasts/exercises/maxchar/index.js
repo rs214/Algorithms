3ddb53c86a047a893820817b057f001c6006c712
rs214 <robertsi0214@gmail.com>
2018-02-20 13:37:09 -0800

    complete maxChar

--- AlgoCasts/exercises/maxchar/index.js
+++ AlgoCasts/exercises/maxchar/index.js
@@ -5,6 +5,20 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  var obj = {};
+  var max = 0;
+  var maxChar = ''
+  for (let char of str) {
+    obj[char] = obj[char] + 1 || 1
+  }
+  for (var key in obj) {
+    if (obj[key] > max) {
+      max = obj[key];
+      maxChar = key;
+    }
+  }
+  return maxChar;
+}
 
 module.exports = maxChar;
