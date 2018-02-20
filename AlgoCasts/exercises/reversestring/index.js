f7e102231dc6199d53ba138ca202a84db23a5a2a
rs214 <robertsi0214@gmail.com>
2018-02-20 10:56:21 -0800

    add final solution to reversedstring

--- AlgoCasts/exercises/reversestring/index.js
+++ AlgoCasts/exercises/reversestring/index.js
@@ -6,25 +6,31 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {
-  var reversedString = ""
-    for (var i = str.length - 1; i >= 0; i--) {
-      reversedString += str[i];
-    }
+// function reverse(str) {
+//   var reversedString = ""
+//     for (var i = str.length - 1; i >= 0; i--) {
+//       reversedString += str[i];
+//     }
 
-  return reversedString;
-}
+//   return reversedString;
+// }
+
+// function reverse(str) {
+//   return str.split('').reverse().join('');
+// }
+
+// function reverse(str) {
+//   let reversedString = ""
+//     for (let character of str) {
+//       reversedString = character + reversedString
+//     }
+//   return reversedString;
+// }
 
 function reverse(str) {
-  return str.split('').reverse().join('');
-}
-
-function reverse(str) {
-  let reversedString = ""
-    for (let character of str) {
-      reversedString = character + reversedString
-    }
-  return reversedString;
+  return str.split('').reduce((reversed, character) => {
+    return character + reversed;
+  }, '')
 }
 
 module.exports = reverse;
