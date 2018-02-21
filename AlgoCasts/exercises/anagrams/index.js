2384135b62c7310f0195b1e43dd9a99e50fb9ac0
rs214 <robertsi0214@gmail.com>
2018-02-21 02:20:20 -0800

    complete anagrams

--- AlgoCasts/exercises/anagrams/index.js
+++ AlgoCasts/exercises/anagrams/index.js
@@ -8,6 +8,35 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function sanitizeString (string) {
+  return string.replace(/[^\w]/g, "").toLowerCase();
+}
+
+function checkIfObjectContains(one, two){
+   for (var i in one) {
+           if (! two.hasOwnProperty(i) || one[i] !== two[i] ) {
+              return false;
+           }
+   }
+   return true;
+}
+
+function anagrams(stringA, stringB) {
+  const A = sanitizeString(stringA);
+  const B = sanitizeString(stringB);
+  var obj1 = {};
+  var obj2 = {};
+
+  for (let char1 of A) {
+    obj1[char1] = obj1[char1] + 1 || 1;
+  }
+  for (let char2 of B) {
+    obj2[char2] = obj2[char2] + 1 || 1;
+  }
+
+  return Object.keys(obj1).length > Object.keys(obj2).length ? checkIfObjectContains(obj1, obj2) : checkIfObjectContains(obj2, obj1)
+}
+
+anagrams('RAIL! SAFETY!', 'fairy tales')
 
 module.exports = anagrams;
