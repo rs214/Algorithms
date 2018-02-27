d56e1fd22b08c58195271622e15bce3991274beb
rs214 <robertsi0214@gmail.com>
2018-02-27 15:05:49 -0800

    add alternative solution to capitalize

--- AlgoCasts/exercises/capitalize/index.js
+++ AlgoCasts/exercises/capitalize/index.js
@@ -18,4 +18,17 @@ function capitalize(str) {
   return newArr.join(' ');
 }
 
+function capitalize(str) {
+  let result = str[0].toUpperCase();
+
+  for (let i = 1; i < str.length; i++) {
+    if (str[i - 1] === " ") {
+      result += str[i].toUpperCase()
+    } else {
+      result += str[i];
+    }
+  }
+  return result;
+}
+
 module.exports = capitalize;
